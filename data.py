e17175d900c2ca9db2a9cd509e5e1a1c8b85e9cf
Ilya Tikhonov <absolute300@yandex.ru>
2026-06-24 23:43:23 +0300

    fix

---
+++ data.py
@@ -0,0 +1,32 @@
+class DataCourier:
+    
+    UNREGISTERED_COURIER_DATA = {
+        "login": "absolutev",
+        "password": "ghbdtn300",
+        "firstName": "Ilya"
+        }
+        
+    COURIER_DATA = {
+        "login": "boomb",
+        "password": "4321",
+        "firstName": "snake"
+        }
+
+
+class DataOrder:
+
+    ORDER_DATA = {
+    "firstName": "Виктор",
+    "lastName": "Боготеев",
+    "address": "Москва, Теплый Стан, 13",
+    "metroStation": "13",
+    "phone": "+7 999 999 99 99",
+    "rentTime": 5,
+    "deliveryDate": "2026-06-06",
+    "comment": "Я всегда вовремя!",
+    "color": [
+        "BLACK"
+        ]
+    }
+
+    
